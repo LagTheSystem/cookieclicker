b1fd3ad02f5c4585cdaab13a560f474af8d662cd
LagTheSystem <53086599+LagTheSystem@users.noreply.github.com>
2022-11-02 15:35:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15889,8 +15889,7 @@ window.onload=function()
 				LoadLang('loc/'+lang+'.js?v='+Game.version,function(){
 					var launch=function(){
 						Game.Launch();
-						if (top!=self) Game.ErrorFrame();
-						else
+						if (top!=self)
 						{
 							console.log('[=== '+choose([
 								'Oh, hello!',
